3503c18c2cc9bf0febdfca45ea0686292fd6643e
unknown <you@example.com>
2023-08-16 07:08:29 +0700

    bai tap day 1

--- day1/demo.js
+++ day1/demo.js
@@ -1,3 +1,4 @@
+//version 1
 //bai 1
 // let a ;
 // let b;
@@ -72,19 +73,177 @@
 //        document.write("Thi hoa hong nhan duoc la: "+hoahong);
 //          }
 //bai 7
-let soPhut = parseInt(prompt("Nhap so phut da su dung"));
-let phi =0;
-let tong = parseFloat();
-let phiCoDinh = 25000;
+// let soPhut = parseInt(prompt("Nhap so phut da su dung"));
+// let phi =0;
+// let tong = parseFloat();
+// let phiCoDinh = 25000;
 
-document.write("Ban da goi "+ soPhut +"phut"+"<br>");
-if(soPhut>200){
-    phi = (soPhut-200)*200+150*400+50*600;
-} else if(soPhut>50){
-    phi = (soPhut-50)*400+50*600;
-} else {
-    phi = soPhut*600;
-}
- tong = 0.01*phi+phiCoDinh;
+// document.write("Ban da goi "+ soPhut +"phut"+"<br>");
+// if(soPhut>200){
+//     phi = (soPhut-200)*200+150*400+50*600;
+// } else if(soPhut>50){
+//     phi = (soPhut-50)*400+50*600;
+// } else {
+//     phi = soPhut*600;
+// }
+//  tong = 0.01*phi+phiCoDinh;
 
-document.write("So tien phai nop cua ban la: "+tong);
+// document.write("So tien phai nop cua ban la: "+tong);
+
+//version 2
+// bai 1
+//  let C = parseFloat(prompt("Nhap vao C"));
+//     let F = C  *  9/5 + 32;
+//     document.write(C+"C= "+F+"F");
+
+//bai 2
+// let m = parseInt(prompt("Nhập vào mét"));
+// let ft =m * 3.2808;
+// document.write(m+" mét = "+ft +"feet ");
+
+//bai 3
+// let a = parseInt(prompt("Nhap canh a: "));
+//   let s = a*a;
+//   document.write("Dien tich hinh vuong la: "+s);
+
+//bai 4
+// let a = parseInt(prompt("Nhap chieu dai"));
+//      let b = parseInt(prompt("Nhap chieu rong"));
+//      let s = a*b;
+//      document.write("Dien tich hinh chu nhat la: "+s);
+
+//bai 5
+// let chieucao = parseInt(prompt("Nhap chieu cao"));
+//      let day = parseInt(prompt("Nhap chieu dai day"));
+//      let s = 1/2*chieucao*day;
+//      document.write("Dien tich tam giac vuong la: "+s);
+
+//bai 6
+// let a = parseInt(prompt("Nhap vao a"));
+//       let b = parseInt(prompt("Nhap vao b"));
+//           if(a==0){
+//               if(b==0){
+//                   alert("Phuong trinh vo so nghiem");
+//               } else {
+//                   alert("Phuong trinh vo nghiem");
+//               }
+//           } else {
+//               alert("Phuong trinh co nghiem: "+-b+"/"+a);
+//           }
+
+//bai 7
+// let a = parseInt(prompt("Nhap a"));
+// document.write("a = "+a+"<br>");
+// let b = parseInt(prompt("Nhap b"));
+// document.write("b = "+b+"<br>");
+// let c = parseInt(prompt("Nhap c"));
+// document.write("c ="+c+"<br>");
+// let delta = b*b-4*a*c;
+// let deta =  Math.sqrt(delta);
+// if(delta<0){
+//     document.write("Phuong trinh vo nghiem");
+// } else if(delta == 0){
+//     document.write('Phương trình có nghiệm kép ' + (-b/(2*a)));
+// } else{
+//     document.write('Phương trình có là nghiệm : ' + ((-b+deta)/(2*a)) + ' và ' + ((-b-deta)/(2*a)) );
+// }
+
+//bai 8
+// let age = parseInt(prompt("Nhập tuổi"));
+//   if(age<120 && age>0){
+//      alert("So tuoi cua ban la: "+age);
+//   } else {
+//       alert("Tuoi khong ton tai.");
+//   }
+
+//bai 9
+// let a = parseFloat(prompt("Nhap a"));
+//     let b = parseFloat(prompt("Nhap b"));
+//     let c = parseFloat(prompt("Nhap c"));
+//         if(a>0 && b>0 && c>0){
+//             if(a+b>c && b+c>a && a+c>b){
+//                 alert("Day la tam giac");
+
+//             } else {
+//                 alert("Day khong phai tam giac");
+//             }
+//         } else {
+//             alert("Day khong phai tam giac");
+//         }
+
+//bai 10
+// let sodien = parseFloat(prompt("Nhap vao so dien"));
+//         document.write("So dien cua ban la: "+sodien+"<br>");
+//         let bac1 = 1678;
+//         let bac2 = 1734;
+//         let bac3 = 2014;
+//         let bac4 = 2536;
+//         let bac5 = 2834;
+//         let bac6 = 2927;
+//         if(sodien<0){
+//             document.write("Vui long nhap lai so dien");
+//         } else if(sodien<=50){
+//             tiendien = sodien*bac1;
+//             document.write("Ban da dung dien trong khoang 0 - 50 kWh so tien ban phai tra la: "+tiendien);
+//         } else if(sodien<=100){
+//             tiendien=50*bac1+((sodien-50)*bac2);
+//             document.write("Ban da dung dien trong khoang 51 - 100 kWh so tien ban phai tra la: "+tiendien);
+//         } else if(sodien<=200){
+//             tiendien=50*bac1+(50*bac2)+((sodien-100)*bac3);
+//             document.write("Ban da dung dien trong khoang 101 - 200 kWh so tien ban phai tra la: "+tiendien);
+//         } else if (sodien<=300) {
+//             tiendien = 50 * bac1 + (50 * bac2) + (100 * bac3) + ((sodien - 200) * bac4);
+//             document.write("Ban da dung dien trong khoang 201 - 300 kWh so tien ban phai tra la: " + tiendien);
+//         } else if(sodien<=400){
+//             tiendien=50*bac1+(50*bac2)+(100*bac3)+(100*bac4)+((sodien-300)*bac5);
+//             document.write("Ban da dung dien trong khoang 301 - 400 kWh so tien ban phai tra la: "+tiendien);
+//         } else if(sodien>400){
+//             tiendien=50*bac1+(50*bac2)+(100*bac3)+(100*bac4)+(100*bac5)+((sodien-400)*bac6);
+//             document.write("Ban da dung dien tren 400 kWh so tien ban phai tra la: "+tiendien);
+
+//         }
+
+//bai 11
+// let thue = parseInt();
+//     let luong = parseFloat(prompt("Nhap so tien luong"));
+//     document.write("Tien luong cua ban la: "+luong+"<br>");
+//     let sothue = parseFloat();
+//     let luongrong = 0;
+//     if(luong>15){
+//         sothue = luong*30/100;
+//         thue = 30;
+//     } else if(luong>=7){
+//         sothue = luong*20/100;
+//         thue = 20;
+//     } else if(luong<7){
+//         sothue = luong*10/100;
+//         thue =10;
+//     }
+//     luongrong = luong - sothue;
+//     document.write("Thue thu nhap: "+thue+"% = "+sothue+"<br>");
+//     document.write("So tien ban nhan duoc la: "+luongrong);
+
+//bai 12
+let soTien =parseFloat(prompt('Nhập số tiền cho vay '));
+    let soThang =parseInt(prompt('Nhập số tháng cho vay '));
+    let thang1 = 3.65, thang3 = 4 , thang6 =5.6, thang12 = 5.7 ,thang18=6,thang24=6,thang36=6.3;
+
+    if (isNaN(soTien) ||isNaN(soThang) || soTien < 0 || soThang < 0 ){
+        document.writeln("Bạn nhập  không hợp lệ");
+    }else{
+        if (soThang == 1 ){
+            document.writeln("Tổng lãi và tiền gửi 1 tháng :"+soTien*thang1);
+        }else if(soThang == 3){
+            document.writeln("Tổng lãi và tiền gửi 3 tháng :"+soTien*thang3);
+        }else if(soThang == 3){
+            document.writeln("Tổng lãi và tiền gửi 6 tháng :"+soTien*thang6);
+        }else if(soThang == 3){
+            document.writeln("Tổng lãi và tiền gửi 12 tháng :"+soTien*thang12);
+        }else if(soThang == 3){
+            document.writeln("Tổng lãi và tiền gửi 18 tháng :"+soTien*thang18);
+        }else if(soThang == 3){
+            document.writeln("Tổng lãi và tiền gửi 24 tháng :"+soTien*thang24);
+        }else {
+            document.writeln("Tổng lãi và tiền gửi những tháng còn lại :"+soTien*thang36);
+        }
+    }
